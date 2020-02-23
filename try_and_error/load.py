eedac612c0958474c1ba816bc832bdce7e89082f
mali <ali-mehdi@live.de>
2020-02-23 14:28:49 +0300

    Remove

--- try_and_error/load.py
+++
@@ -1,7 +0,0 @@
-import json
-
-with open('/Users/mali/PycharmProjects/pykeen_experimental_results/try_and_error/config.json') as json_file:
-    data = json.load(json_file)
-    bs = data['ablation']['evaluation_kwargs']['batch_size']
-    print(bs)
-    print(bs is None)
