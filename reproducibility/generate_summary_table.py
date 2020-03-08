6419f0d04d26c3df22a708f1f4c8c583a7f1e675
Charles Tapley Hoyt <cthoyt@gmail.com>
2020-03-08 23:10:24 +0100

    Update pdf output

--- reproducibility/generate_summary_table.py
+++ reproducibility/generate_summary_table.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import os
+from collections import defaultdict
 
 import pandas as pd
 from jinja2 import Environment, FileSystemLoader
@@ -13,9 +14,7 @@ os.makedirs(SUMMARIES, exist_ok=True)
 
 
 def main():
-    df = get_df()
-
-    for dataset, dataset_df in df.groupby('dataset'):
+    for dataset, dataset_df in get_df().groupby('dataset'):
         rows = []
         for model, model_df in dataset_df.groupby('model'):
             for column in model_df.columns:
@@ -25,45 +24,108 @@ def main():
                 std = model_df[column].std()
                 if '.mean_rank.' in column or column.startswith('times'):
                     fmt = '5.2f'
-                    factor = 1
+                    std_fmt = '.2f'
+                    factor = 1.0
                 else:
-                    fmt = '.2f'
-                    factor = 100
+                    std_fmt = fmt = '.2f'
+                    factor = 100.0
 
                 _, column = column.split('.', 1)
-                t = f'{factor * mean:{fmt}} ± {factor * std:{fmt}}'
+                t = f'{(factor * mean):{fmt}} ± {(factor * std):{std_fmt}}'
                 rows.append((model, column, t))
 
         x = pd.DataFrame(rows, columns=['model', 'columns', 'values'])
         x = x.set_index(['model', 'columns']).unstack(level=-1).reset_index()
-        new_columns = ['model'] + [
-                                      column.replace('_', ' ').replace('.', ' ')
-                                      for column in x.columns.get_level_values(1)
-                                  ][1:]
-        x.columns = new_columns
+        x.columns = ['model'] + list(x.columns.get_level_values(1))[1:]
+        x = x[[col for col in list(x.columns) if col not in {'training', 'evaluation'}]]
+        x.columns = get_renamed_columns(x)
+        x = get_reordered_df(x)
 
         # Save as TSV
         x.to_csv(os.path.join(SUMMARIES, f'{dataset}.tsv'), sep='\t', index=False)
 
         # Save as Latex table
-        table_latex = x.to_latex(index=False, escape=False).replace('±', '$\\pm$')
+        table_latex = x.to_latex(
+            index=False, escape=False, column_format='r' * len(x.columns)
+        )
+        table_latex = table_latex.replace('±', '$\\pm$')
         with open(os.path.join(SUMMARIES, f'{dataset}_table.tex'), 'w') as file:
             print(table_latex, file=file)
 
-        loader = FileSystemLoader(os.path.join(HERE, 'templates'))
-        environment = Environment(
-            autoescape=False,
-            loader=loader,
-            trim_blocks=False,
-        )
-        r = environment.get_template('table_template.tex').render(table=table_latex, dataset=dataset)
-        with open(os.path.join(SUMMARIES, f'{dataset}.tex'), 'w') as file:
-            print(r, file=file)
+        write_latex(dataset, x)
 
-        try:
-            os.system(f"cd {SUMMARIES} && latexmk -pdf {dataset}")
-        except Exception:
-            print('Was not able to build PDF.')
+
+def write_latex(dataset: str, df: pd.DataFrame) -> None:
+    loader = FileSystemLoader(os.path.join(HERE, 'templates'))
+    environment = Environment(
+        autoescape=False,
+        loader=loader,
+        trim_blocks=False,
+    )
+    template = environment.get_template('table_template.tex')
+
+    z = defaultdict(list)
+    for measurement, y in df.columns[1:]:
+        z[measurement].append((measurement, y))
+
+    tables = []
+    for measurement, y in z.items():
+        sub_df = df[[('', 'model')] + y]
+        sub_df.columns = list(sub_df.columns.get_level_values(1))
+        table_latex = sub_df.to_latex(
+            index=False,
+            escape=False,
+            column_format='r' * len(sub_df.columns),
+            caption=f'{dataset} - {measurement}',
+        )
+        table_latex = table_latex.replace('±', '$\\pm$')
+        tables.append((measurement, table_latex))
+
+    with open(os.path.join(SUMMARIES, f'{dataset}.tex'), 'w') as file:
+        print(template.render(tables=tables, dataset=dataset), file=file)
+
+    try:
+        os.system(f"cd {SUMMARIES} && latexmk -pdf {dataset}")
+    except OSError:
+        print('Was not able to build PDF.')
+
+
+def get_renamed_columns(df: pd.DataFrame):
+    rv = []
+    for column in df.columns:
+        column = column.replace('_', ' ').replace('.', ' ')
+
+        if column.startswith('hits at k'):
+            column_split = list(column.split(' '))
+            column = f'hits@{column_split[-1]}'
+            t = column_split[-2]
+        elif column.startswith('mean reciprocal rank'):
+            t = column.split(' ')[-1]
+            column = 'MRR'
+        elif column.startswith('mean rank'):
+            t = column.split(' ')[-1]
+            column = 'MR'
+        elif column.startswith('adjusted mean rank'):
+            column = 'AMR'
+            t = 'avg'
+        elif column == 'model':
+            t = ''
+        else:
+            t = 'time'
+        rv.append((t, column))
+    return pd.MultiIndex.from_tuples(rv)
+
+
+def get_reordered_df(df: pd.DataFrame):
+    r = defaultdict(list)
+    for x, y in df.columns:
+        r[x].append(y)
+
+    reordered = []
+    for x in ['', 'avg', 'best', 'worst']:
+        for y in r[x]:
+            reordered.append((x, y))
+    return df[reordered]
 
 
 if __name__ == '__main__':
