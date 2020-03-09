a732b8f73e9dc76516318afe9382da661ab090eb
Charles Tapley Hoyt <cthoyt@gmail.com>
2020-03-09 17:47:03 +0100

    Update PDF Export

--- reproducibility/generate_summary_table.py
+++ reproducibility/generate_summary_table.py
@@ -14,6 +14,7 @@ os.makedirs(SUMMARIES, exist_ok=True)
 
 
 def main():
+    all_tables = []
     for dataset, dataset_df in get_df().groupby('dataset'):
         rows = []
         for model, model_df in dataset_df.groupby('model'):
@@ -35,27 +36,32 @@ def main():
                 rows.append((model, column, t))
 
         x = pd.DataFrame(rows, columns=['model', 'columns', 'values'])
-        x = x.set_index(['model', 'columns']).unstack(level=-1).reset_index()
-        x.columns = ['model'] + list(x.columns.get_level_values(1))[1:]
+        x = x.set_index(['model', 'columns']).unstack(level=-1).reset_index().set_index('model')
+        x.columns = x.columns.get_level_values(1)
         x = x[[col for col in list(x.columns) if col not in {'training', 'evaluation'}]]
         x.columns = get_renamed_columns(x)
         x = get_reordered_df(x)
 
         # Save as TSV
-        x.to_csv(os.path.join(SUMMARIES, f'{dataset}.tsv'), sep='\t', index=False)
+        x.to_csv(os.path.join(SUMMARIES, f'{dataset}.tsv'), sep='\t', index=True)
 
         # Save as Latex table
         table_latex = x.to_latex(
-            index=False, escape=False, column_format='r' * len(x.columns)
+            index=True,
+            escape=False,
+            column_format='l' + ('r' * len(x.columns)),
+            bold_rows=True,
         )
         table_latex = table_latex.replace('±', '$\\pm$')
         with open(os.path.join(SUMMARIES, f'{dataset}_table.tex'), 'w') as file:
             print(table_latex, file=file)
 
-        write_latex(dataset, x)
+        all_tables.append((dataset, x))
+
+    write_pdfs(all_tables)
 
 
-def write_latex(dataset: str, df: pd.DataFrame) -> None:
+def write_pdfs(all_tables) -> None:
     loader = FileSystemLoader(os.path.join(HERE, 'templates'))
     environment = Environment(
         autoescape=False,
@@ -64,30 +70,49 @@ def write_latex(dataset: str, df: pd.DataFrame) -> None:
     )
     template = environment.get_template('table_template.tex')
 
-    z = defaultdict(list)
-    for measurement, y in df.columns[1:]:
-        z[measurement].append((measurement, y))
+    table_results = []
 
-    tables = []
-    for measurement, y in z.items():
-        sub_df = df[[('', 'model')] + y]
-        sub_df.columns = list(sub_df.columns.get_level_values(1))
-        table_latex = sub_df.to_latex(
-            index=False,
-            escape=False,
-            column_format='r' * len(sub_df.columns),
-            caption=f'{dataset} - {measurement}',
-        )
-        table_latex = table_latex.replace('±', '$\\pm$')
-        tables.append((measurement, table_latex))
+    for dataset, df in all_tables:
+        z = defaultdict(list)
+        for measurement, y in df.columns:
+            z[measurement].append((measurement, y))
 
-    with open(os.path.join(SUMMARIES, f'{dataset}.tex'), 'w') as file:
-        print(template.render(tables=tables, dataset=dataset), file=file)
+        tables = {}
+        for measurement, y in z.items():
+            sub_df = df[y]
+            sub_df.index.name = None
+            sub_df.columns = list(sub_df.columns.get_level_values(1))
+
+            hits_df = sub_df[['hits@1', 'hits@3', 'hits@5', 'hits@10']]
+            if measurement == 'avg':
+                ranked_df = sub_df[['MR', 'MRR', 'AMR']]
+            else:
+                ranked_df = sub_df[['MR', 'MRR']]
+
+            tables[measurement, 'hits'] = get_latex(hits_df)
+            tables[measurement, 'rank'] = get_latex(ranked_df)
+
+        table_results.append((dataset, tables))
+
+    tex = template.render(table_results=table_results)
+    with open(os.path.join(SUMMARIES, f'results.tex'), 'w') as file:
+        print(tex, file=file)
 
     try:
-        os.system(f"cd {SUMMARIES} && latexmk -pdf {dataset}")
+        os.system(f"cd {SUMMARIES} && latexmk -pdf results")
+        os.system(f"cd {SUMMARIES} && rm *.log && rm *.aux && rm *.fls && rm *.fdb_latexmk")
     except OSError:
         print('Was not able to build PDF.')
+
+
+def get_latex(df: pd.DataFrame) -> str:
+    table_latex = df.to_latex(
+        index=True,
+        escape=False,
+        column_format='l' + ('r' * len(df.columns)),
+        bold_rows=True,
+    )
+    return table_latex.replace('±', '$\\pm$')
 
 
 def get_renamed_columns(df: pd.DataFrame):
