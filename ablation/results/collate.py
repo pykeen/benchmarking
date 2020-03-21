53f3f11930ccbeb2b29ab052f27c50e50224aaec
Charles Tapley Hoyt <cthoyt@gmail.com>
2020-03-21 19:19:38 +0100

    Just like Duke

--- ablation/results/collate.py
+++ ablation/results/collate.py
@@ -156,7 +156,7 @@ def make_plots(*, df: pd.DataFrame, target_header: str):
             sns.boxplot(data=sub_df, x=ablation_header, y=target_header, ax=ax, order=sub_df_agg.index)
             sns.swarmplot(data=sub_df, x=ablation_header, y=target_header, ax=ax, linewidth=1.0, order=sub_df_agg.index)
 
-            ax.set_title(ablation_header.replace('_', ' ').capitalize())
+            ax.set_title(ablation_header.replace('_', ' ').title())
             ax.set_xlabel('')
 
             for tick in ax.get_xticklabels():
@@ -172,12 +172,7 @@ def make_plots(*, df: pd.DataFrame, target_header: str):
               ' regenerate this README when new data is available', file=file)
         for dataset in df.dataset.unique():
             print(f'## {dataset}\n', file=file)
-            print(
-                f'<img src="results/_results/{dataset}/{dataset}.png"'
-                f' alt="{dataset}"'
-                f' height="675" />\n',
-                file=file,
-            )
+            print(f'<img src="results/_results/{dataset}/{dataset}.png" alt="{dataset}"/>\n', file=file)
 
 
 def main():
