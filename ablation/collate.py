fa576d297ad127d251a9903069b363048c4812dc
Max Berrendorf <berrendorf@dbs.ifi.lmu.de>
2020-06-15 13:19:45 +0200

    Add additional metrics to collation

--- ablation/collate.py
+++ ablation/collate.py
@@ -1,5 +1,6 @@
 import logging
 import os
+from typing import Collection, Optional
 
 import click
 import pandas as pd
@@ -24,12 +25,13 @@ def read_collation() -> pd.DataFrame:
     return pykeen_report.utils.read_ablation_collation(COLLATION_PATH)
 
 
-def collate(key: str = 'hits@10') -> pd.DataFrame:
+def collate(key: str = 'hits@10', additional_metrics: Optional[Collection[str]] = None) -> pd.DataFrame:
     """Collate all results for a given metric."""
     return pykeen_report.utils.collate_ablation(
         results_directory=RESULTS,
         output_path=COLLATION_PATH,
         key=key,
+        additional_metrics=additional_metrics,
     )
 
 
